d8662aa4e413dfcdef3669f761e2205e5a127c1e
Donia Alhosin <doniaelhussien@gmail.com>
2025-11-25 00:26:46 +0200

    Revert " Product and Wishlist"
    This reverts commit 52ff230d68539d6e4d78a3cc0b565d9a17ebbed3.

--- src/test/java/tests/ProductTests.java
+++ src/test/java/tests/ProductTests.java
@@ -1,45 +1,4 @@
 package tests;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import pages.ProductPage;
-import pages.WishlistPage;
-
 public class ProductTests {
-
-    @Test
-    public void verifyProductDetails() {
-        ProductPage product = new ProductPage(driver);
-
-        product.openProductPage("MacBook");
-
-        Assert.assertTrue(product.getProductName().contains("MacBook"), "Product name is incorrect");
-        Assert.assertTrue(product.getProductPrice().startsWith("$"), "Price format incorrect");
-        Assert.assertTrue(product.isProductImageDisplayed(), "Product image not visible");
-    }
-
-    @Test
-    public void addProductToCart() {
-        ProductPage product = new ProductPage(driver);
-
-        product.openProductPage("MacBook");
-        product.addToCart();
-
-        Assert.assertTrue(product.isSuccessMessageDisplayed(), "Success message not shown");
-    }
-
-    @Test
-    public void addProductToWishlist() {
-        ProductPage product = new ProductPage(driver);
-        WishlistPage wishlist = new WishlistPage(driver);
-
-        product.openProductPage("MacBook");
-        product.addToWishlist();
-
-        Assert.assertTrue(product.isSuccessMessageDisplayed(), "Success message not displayed");
-
-        wishlist.openWishlistPage();
-        Assert.assertTrue(wishlist.isProductInWishlist("MacBook"), "Product not found in wishlist");
-    }
-
 }
